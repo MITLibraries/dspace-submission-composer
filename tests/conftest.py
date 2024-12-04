00a01068079998f9e0ab1f8a11cbf8506ef7b229
Eric Hanson <ehanson@mit.edu>
2024-12-04 15:13:10 -0500

    Add Config class
    Why these changes are being introduced:
    * The application needs a class to manage config values and a stream variable to store the application logs for an email sent at the end of each run.
    
    How this addresses that need:
    * Add Config class with corresponding unit tests and fixtures for the methods
    * Update cli.py with calls to Config methods and stream variable
    * Update README.md
    * Update pyproject.toml
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1108

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,28 @@
+from io import StringIO
+
 import pytest
 from click.testing import CliRunner
+
+from dsc.config import Config
 
 
 @pytest.fixture(autouse=True)
 def _test_env(monkeypatch):
     monkeypatch.setenv("SENTRY_DSN", "None")
     monkeypatch.setenv("WORKSPACE", "test")
+    monkeypatch.setenv("AWS_REGION_NAME", "us-east-1")
+
+
+@pytest.fixture
+def config_instance() -> Config:
+    return Config()
 
 
 @pytest.fixture
 def runner():
     return CliRunner()
+
+
+@pytest.fixture
+def stream():
+    return StringIO()
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,37 +1,45 @@
 import logging
 
-from dsc.config import configure_logger, configure_sentry
+import pytest
 
 
-def test_configure_logger_not_verbose():
+def test_check_required_env_vars(monkeypatch, config_instance):
+    monkeypatch.delenv("WORKSPACE")
+    with pytest.raises(OSError, match="Missing required environment variables:"):
+        config_instance.check_required_env_vars()
+
+
+def test_configure_logger_not_verbose(config_instance, stream):
     logger = logging.getLogger(__name__)
-    result = configure_logger(logger, verbose=False)
-    info_log_level = 20
-    assert logger.getEffectiveLevel() == info_log_level
+    result = config_instance.configure_logger(logger, stream, verbose=False)
+    assert logger.getEffectiveLevel() == logging.INFO
     assert result == "Logger 'tests.test_config' configured with level=INFO"
+    stream.seek(0)
+    assert next(stream) == "INFO\n"
 
 
-def test_configure_logger_verbose():
+def test_configure_logger_verbose(config_instance, stream):
     logger = logging.getLogger(__name__)
-    result = configure_logger(logger, verbose=True)
-    debug_log_level = 10
-    assert logger.getEffectiveLevel() == debug_log_level
+    result = config_instance.configure_logger(logger, stream, verbose=True)
+    assert logger.getEffectiveLevel() == logging.DEBUG
     assert result == "Logger 'tests.test_config' configured with level=DEBUG"
+    stream.seek(0)
+    assert next(stream) == "DEBUG\n"
 
 
-def test_configure_sentry_no_env_variable(monkeypatch):
+def test_configure_sentry_no_env_variable(monkeypatch, config_instance):
     monkeypatch.delenv("SENTRY_DSN", raising=False)
-    result = configure_sentry()
+    result = config_instance.configure_sentry()
     assert result == "No Sentry DSN found, exceptions will not be sent to Sentry"
 
 
-def test_configure_sentry_env_variable_is_none(monkeypatch):
+def test_configure_sentry_env_variable_is_none(monkeypatch, config_instance):
     monkeypatch.setenv("SENTRY_DSN", "None")
-    result = configure_sentry()
+    result = config_instance.configure_sentry()
     assert result == "No Sentry DSN found, exceptions will not be sent to Sentry"
 
 
-def test_configure_sentry_env_variable_is_dsn(monkeypatch):
+def test_configure_sentry_env_variable_is_dsn(monkeypatch, config_instance):
     monkeypatch.setenv("SENTRY_DSN", "https://1234567890@00000.ingest.sentry.io/123456")
-    result = configure_sentry()
+    result = config_instance.configure_sentry()
     assert result == "Sentry DSN found, exceptions will be sent to Sentry with env=test"
