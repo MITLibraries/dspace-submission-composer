ab10904abea8e3cfcc3833e5464ec75db3cf5652
jonavellecuerdo <cuerdojonavelle@gmail.com>
2025-06-09 16:45:36 -0400

    Improve methods for processing result messages
    Why these changes are being introduced:
    * Clear invalid result messages from the output SQS queue and
    clarify validation rules for DSS result messages
    
    How this addresses that need:
    * Define JSON Schemas to validate content of 'MessageAttributes'
    and 'Body' of DSS result messages
    * Create a method on base Workflow class for validating and
    parsing result messages
    
    Side effects of this change:
    * None
    
    Relevant ticket(s)
    * https://mitlibraries.atlassian.net/browse/IN-1323

---
+++ dsc/utilities/validate/schemas.py
@@ -0,0 +1,68 @@
+RESULT_MESSAGE_ATTRIBUTES = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "DSS Result Message - MessageAttributes",
+    "description": "Content of result message 'MessageAttributes' from DSS",
+    "type": "object",
+    "properties": {
+        "PackageID": {
+            "type": "object",
+            "properties": {
+                "DataType": {"type": "string", "const": "String"},
+                "StringValue": {"type": "string"},
+            },
+            "required": ["DataType", "StringValue"],
+        },
+        "SubmissionSource": {
+            "type": "object",
+            "properties": {
+                "DataType": {"type": "string", "const": "String"},
+                "StringValue": {"type": "string"},
+            },
+            "required": ["DataType", "StringValue"],
+        },
+    },
+    "required": ["PackageID", "SubmissionSource"],
+}
+
+RESULT_MESSAGE_BODY = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "DSS Result Message - Body",
+    "description": "Content of result message 'Body' from DSS",
+    "type": "object",
+    "properties": {
+        "ResultType": {"type": "string"},
+        "ItemHandle": {"type": "string"},
+        "lastModified": {"type": "string"},
+        "Bitstreams": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "BitstreamName": {"type": "string"},
+                    "BitstreamUUID": {"type": "string"},
+                    "BitstreamChecksum": {
+                        "type": "object",
+                        "properties": {
+                            "value": {"type": "string"},
+                            "checkSumAlgorithm": {"type": "string"},
+                        },
+                    },
+                },
+            },
+        },
+        "ErrorTimestamp": {"type": "string"},
+        "ErrorInfo": {"type": "string"},
+        "DSpaceResponse": {"type": "string"},
+        "ExceptionTraceback": {"type": "array", "items": {"type": "string"}},
+    },
+    "if": {"properties": {"ResultType": {"const": "success"}}},
+    "then": {"required": ["ItemHandle", "lastModified", "Bitstreams"]},
+    "else": {
+        "required": [
+            "ErrorTimestamp",
+            "ErrorInfo",
+            "DSpaceResponse",
+            "ExceptionTraceback",
+        ]
+    },
+}
