d91b99c9db93bfb834a0abc1fc76e2bbce156c81
Jonavelle Cuerdo <cuerdojonavelle@gmail.com>
2025-07-08 16:24:53 -0400

    Remove Demo workflow (#186)
    Why these changes are being introduced:
    * The Demo workflow was always intended to be temporary and was
    initially created for testing parts of DSC with 'wiley-deposits'
    infrastructure. Now that DSC infrastructure deployed in Dev
    (and soon to be deployed in Stage and Prod), the Demo workflow
    can be deprecated.
    
    How this addresses that need:
    * Remove metadata mapping JSON file for Demo workflow
    * Remove Demo workflow module
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1167

--- dsc/workflows/__init__.py
+++ dsc/workflows/__init__.py
@@ -5,8 +5,7 @@ All primary functions used by CLI are importable from here.
 
 from dsc.workflows.base import Workflow, WorkflowEvents
 from dsc.workflows.base.simple_csv import SimpleCSV
-from dsc.workflows.demo import Demo
 from dsc.workflows.opencourseware import OpenCourseWare
 from dsc.workflows.sccs import SCCS
 
-__all__ = ["SCCS", "Demo", "OpenCourseWare", "SimpleCSV", "Workflow", "WorkflowEvents"]
+__all__ = ["SCCS", "OpenCourseWare", "SimpleCSV", "Workflow", "WorkflowEvents"]
