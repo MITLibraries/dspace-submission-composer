d9e8ad3ef3183d27483069148a82b6d7d750907a
Eric Hanson <ehanson@mit.edu>
2025-08-26 14:08:00 -0400

    Updates based on discussion in PR # 198
    * Create successful_item_submissions variable from list comprehension
    * Replace csv module with pandas

--- dsc/workflows/archivesspace.py
+++ dsc/workflows/archivesspace.py
@@ -1,6 +1,6 @@
-import csv
 import logging
 
+import pandas as pd
 import smart_open
 
 from dsc.db.models import ItemSubmissionStatus
@@ -41,12 +41,13 @@ class ArchivesSpace(SimpleCSV):
         handle_uri_mapping = {}
 
         # find item submissions that were successfully ingested on the current run
-        for item_submission in [
+        successful_item_submissions: list[ItemSubmission] = [
             item_submission
             for item_submission in ItemSubmission.get_batch(self.batch_id)
             if item_submission.last_run_date == self.run_date
             and item_submission.status == ItemSubmissionStatus.INGEST_SUCCESS
-        ]:
+        ]
+        for item_submission in successful_item_submissions:
             handle_uri_mapping[item_submission.source_system_identifier] = (
                 item_submission.dspace_handle
                 if item_submission.dspace_handle
@@ -58,14 +59,26 @@ class ArchivesSpace(SimpleCSV):
             )
             return
 
+        # create dataframe
+        df = pd.DataFrame(
+            [
+                {
+                    "ao_uri": item.source_system_identifier,
+                    "dspace_handle": (
+                        item.dspace_handle
+                        if item.dspace_handle
+                        else "DSpace handle not set, possible error"
+                    ),
+                }
+                for item in successful_item_submissions
+            ]
+        )
+
         # create report in S3 bucket
         with smart_open.open(
             f"s3://{self.output_path}/{self.batch_id}-{run_date_str}.csv", "w"
         ) as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow(["ao_uri", "dspace_handle"])
-            for ao_uri, dspace_handle in handle_uri_mapping.items():
-                writer.writerow([ao_uri, dspace_handle])
+            df.to_csv(csv_file, index=False)
 
         logger.debug(
             f"Completed ingest report for batch '{self.batch_id}' on run date"
