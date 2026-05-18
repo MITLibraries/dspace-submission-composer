bbb2e4b7c932ada96ca444691a6cb0b69b6d03c1
Jonavelle Cuerdo <cuerdojonavelle@gmail.com>
2026-05-18 12:37:42 -0400

    Add XML namespace for the SRU protocol

--- dsc/workflows/digitized_theses/__init__.py
+++ dsc/workflows/digitized_theses/__init__.py
@@ -1,3 +1,3 @@
-from dsc.workflows.digitized_theses.transformer import DigitizedThesesTransformer
+from dsc.workflows.digitized_theses.transformer import DigitizedThesesTransformer, NSMAP
 
-__all__ = ["DigitizedThesesTransformer"]
+__all__ = ["DigitizedThesesTransformer", "NSMAP"]
