be72038d24e426c54c7878a3ee96a2a612bb471c
Jonavelle Cuerdo <cuerdojonavelle@gmail.com>
2026-08-25 20:03:27 -0400

    Implement Wiley workflow 'create' step
    Why these changes are being introduced:
    * The Wiley workflow requires a batch creation process that
    must be capable of performing the following functions:
    
    1. Accept a CSV file of DOIs as input
    2. Can download bitstreams (PDFs) from Wiley via API
    3. Can download metadata from Crossref
    4. Can determine whether a DOI was seen from a previous
    batch run for Wiley
    
    How this addresses that need:
    * Add required env vars for Wiley workflow
    * Add ItemSubmission method for retrieving all submissions
    for a given workflow
    * Add download method to S3 client
    * Create Wiley workflow module
    
    Side effects of this change:
    * This requires the addition of two new env vars required
    by the Wiley workflow:
    - WILEY_METADATA_API_URL
    - WILEY_BITSTREAM_API_URL
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1097

--- dsc/utils/aws/__init__.py
+++ dsc/utils/aws/__init__.py
@@ -1,6 +1,13 @@
 from dsc.utils.aws.metrics import Metric, MetricsClient
-from dsc.utils.aws.s3 import S3Client
+from dsc.utils.aws.s3 import S3Client, run_aws_cli_sync
 from dsc.utils.aws.ses import SESClient
 from dsc.utils.aws.sqs import SQSClient
 
-__all__ = ["Metric", "MetricsClient", "S3Client", "SESClient", "SQSClient"]
+__all__ = [
+    "Metric",
+    "MetricsClient",
+    "S3Client",
+    "SESClient",
+    "SQSClient",
+    "run_aws_cli_sync",
+]
---
+++ dsc/workflows/wiley/__init__.py
@@ -0,0 +1,4 @@
+from dsc.workflows.wiley.transformer import WileyTransformer
+from dsc.workflows.wiley.workflow import Wiley
+
+__all__ = ["Wiley", "WileyTransformer"]
