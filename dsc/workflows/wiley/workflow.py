b234fecec61a9f68d4f748422042630eb3938aa7
Jonavelle Cuerdo <cuerdojonavelle@gmail.com>
2026-08-31 16:25:09 -0400

    Implement Wiley workflow 'submit' process

--- dsc/workflows/wiley/workflow.py
+++ dsc/workflows/wiley/workflow.py
@@ -3,12 +3,14 @@ import json
 import logging
 import os
 import tempfile
+from collections import defaultdict
 from collections.abc import Iterator
 from pathlib import Path
 from typing import Any
 
 import pandas as pd
 import requests
+import smart_open
 
 from dsc import exceptions
 from dsc.config import Config
@@ -276,3 +278,140 @@ class Wiley(Workflow):
         until a batch is ready for submission.
         """
         return f"{batch_id}-{self.run_date.strftime('%Y%m%dT%H%M%SZ')}"
+
+    def submit_items(self, collection_handle: str | None = None) -> list:
+        logger.info(
+            f"Submitting messages to the DSS input queue '{CONFIG.sqs_queue_dss_input}' "
+            f"for batch '{self.batch_id}'"
+        )
+
+        manifest = self._load_batch_manifest()
+
+        items = []
+        for item_submission in ItemSubmission.get_batch(self.batch_id):
+            self.submission_summary["total"] += 1
+            item_submission.last_run_date = self.run_date
+            logger.debug(
+                f"Preparing submission for item: {item_submission.item_identifier}"
+            )
+
+            # validate whether a message should be sent for this item submission
+            if not item_submission.ready_to_submit():
+                self.submission_summary["skipped"] += 1
+                continue
+            try:
+                # get item metadata
+                item_metadata = self._get_transformed_metadata(
+                    source_metadata_file=manifest[item_submission.item_identifier][
+                        "metadata_file"
+                    ],
+                )
+
+                # prepare submission assets
+                item_submission.prepare_dspace_metadata(
+                    item_metadata=item_metadata,
+                    s3_bucket=self.s3_bucket,
+                    batch_path=self.batch_path,
+                )
+
+                # attach bitstreams from manifest
+                item_submission.bitstream_s3_uris = manifest[
+                    item_submission.item_identifier
+                ]["bitstream_files"]
+
+                # attach collection handle
+                item_submission.collection_handle = collection_handle
+
+                # send submission message
+                response = item_submission.send_submission_message(
+                    submission_source=self.workflow_name,
+                    output_queue=self.output_queue,
+                    submission_system=self.submission_system,
+                    collection_handle=item_submission.collection_handle,
+                )
+
+                # Record details of the item submission message
+                item_data = {
+                    "item_identifier": item_submission.item_identifier,
+                    "message_id": response["MessageId"],
+                }
+                items.append(item_data)
+                self.submission_summary["submitted"] += 1
+
+                logger.info(f"Sent item submission message: {item_data['message_id']}")
+
+                # Set status in DynamoDB
+                item_submission.status = ItemSubmissionStatus.SUBMIT_SUCCESS
+                item_submission.status_details = None
+                item_submission.submit_attempts += 1
+                item_submission.upsert_db()
+                self._publish_count_metric(
+                    "item_submitted", f"item {item_submission.item_identifier}"
+                )
+
+            except Exception as exception:  # noqa: BLE001
+                self.submission_summary["errors"] += 1
+                item_submission.status = ItemSubmissionStatus.SUBMIT_FAILED
+                item_submission.status_details = str(exception)
+                item_submission.submit_attempts += 1
+                item_submission.upsert_db()
+                self._publish_count_metric(
+                    "submission_error", f"item {item_submission.item_identifier}"
+                )
+
+        logger.info(
+            f"Submitted messages to the DSS input queue '{CONFIG.sqs_queue_dss_input}' "
+            f"for batch '{self.batch_id}': {json.dumps(self.submission_summary)}"
+        )
+
+        return items
+
+    def _load_batch_manifest(self) -> dict:
+        """Create a manifest for a batch of item submissions.
+
+        This method "walks" the contents of the batch in S3, examining contents
+        of the new- and replacement-theses subfolders. The method returns
+        a dictionary where the keys = item identifiers and the value
+        is a dict with important meta about the item submission: thesis type and
+        the S3 URIs for the metadata file and the associated bitstream.
+        """
+        manifest: defaultdict = defaultdict(dict)
+
+        s3_client = S3Client()
+
+        for file in s3_client.files_iter(
+            bucket=self.s3_bucket, prefix=f"{self.batch_path}"
+        ):
+            # in S3, the slash in the item identifier (the DOI) is replaced with a dash
+            item_identifier = file.rsplit("/", maxsplit=2)[1].replace("-", "/")
+
+            # add item submission ID to manifest
+            if item_identifier not in manifest:
+                manifest[item_identifier]
+
+            if file.endswith(".json"):
+                manifest[item_identifier]["metadata_file"] = file
+            if file.endswith(".pdf"):
+                manifest[item_identifier]["bitstream_file"].setdefault(
+                    "bitstream_files", []
+                ).append(file)
+
+        return manifest
+
+    def _get_transformed_metadata(self, source_metadata_file: str) -> dict:
+        """Get transformed metadata for an item submission.
+
+        This method expects a filepath to an Alma MARC XML file.
+        The contents of the XML file are passed to DigitizedTheses.metadata_transformer
+        as bytes. The transformer returns a dictionary with key-value
+        pairs of Qualified Dublin Core (QDC) metadata, where the value is a
+        list of values for each field entry.
+
+        The method returns a dictionary with the QDC metadata, and
+        additional entries for the dc.description.provenance
+        and dspace.imported metadata fields.
+        """
+        with smart_open.open(source_metadata_file, "rb") as file:
+            source_metadata = file.read()
+
+        return self.metadata_transformer.transform(source_metadata)
